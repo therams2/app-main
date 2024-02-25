1175ec7f985b695cd808c52c87109df354e701cf
rjmedina <rams3200@outlook.com>
2024-02-25 15:50:54 -0600

    Revert "Update ShowCatArticulos.php"
    This reverts commit a9c2b9d9ceca098b97863715ef06bcb03bc9956e.

--- app/Http/Livewire/Adq/ShowCatArticulos.php
+++ app/Http/Livewire/Adq/ShowCatArticulos.php
@@ -39,14 +39,14 @@ class ShowCatArticulos extends Component
                         'adq_cat_articulos.code',
                         'acc.nombre as nombre_cat',
                         'acc.id as id_cat')
-        ->join('adq_cat_categorias as acc', 'acc.id', '=', 'adq_cat_articulos.id')
+        ->leftjoin('adq_cat_categorias as acc', 'acc.id', '=', 'adq_cat_articulos.id')
         ->where('adq_cat_articulos.nombre','like', '%'        . $this->search . '%')
         ->orwhere('adq_cat_articulos.descripcion','like', '%' . $this->search . '%')
         ->orwhere('cantidad','like', '%'    . $this->search . '%')
         ->orwhere('precio','like', '%'      . $this->search . '%')
         ->orwhere('costo_ini','like', '%'   . $this->search . '%')
         ->orwhere('code','like', '%'        . $this->search . '%')
-        ->orderByDesc('adq_cat_articulos.id') 
+        ->orderByDesc('id') 
         ->get(); 
         $getCategorias = cat_categorias::select()->get();
         
